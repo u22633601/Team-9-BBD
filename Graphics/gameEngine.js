a2f05e31a1b3854d5f93822f433f1f54830b9ab8
Shen-2102687 <100612118+Shen-Reddy@users.noreply.github.com>
2024-07-01 15:43:56 +0200

    added movement to ball

--- Graphics/gameEngine.js
+++ Graphics/gameEngine.js
@@ -1,80 +1,119 @@
-import {Ball} from '../MazeLogic/Ball';
-
-//var GamePiece;
+var GamePiece;
 var Obstacles = [];
 var testWall;
 var testWall2;
-var ball;
 
 function startGame() {
     //GameArea.start();
     //GamePiece = new createWall(30, 30, "blue", 10, 120);
-    //GamePiece = new createPlayer(60, 60, 15, 0, 2 * Math.PI, "blue");
+    GamePiece = new createPlayer(60, 60, 15, 0, 2 * Math.PI, "blue");
     testWall = new createWall(30, 90, "red", 70, 90);
     testWall2 = new createWall(30, 90, "red", 200, 90);
-    ball = new Ball(90,90,20);
     GameArea.start();
   }
   
-  var GameArea = {
-    canvas : document.createElement("canvas"),
-    start : function() {
-      this.canvas.width = 720;
-      this.canvas.height = 600;
-      this.context = this.canvas.getContext("2d");
-      document.body.insertBefore(this.canvas, document.body.childNodes[0]);
-      this.frameNo = 0;
-      this.interval = setInterval(updateGameArea, 20);
-    },
-    clear : function() {
-      this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    }
+var GameArea = {
+  canvas : document.createElement("canvas"),
+  start : function() {
+    this.canvas.width = 720;
+    this.canvas.height = 600;
+    this.context = this.canvas.getContext("2d");
+    document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+    this.frameNo = 0;
+    this.interval = setInterval(updateGameArea, 20);
+  },
+  clear : function() {
+    this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   }
+}
 
-  function everyinterval(n) {
-    if ((GameArea.frameNo / n) % 1 == 0) {return true;}
-    return false;
-  }
-  
-  function createWall(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
-    this.x = x;
-    this.y = y;
-    this.update = function(){
-      ctx = GameArea.context;
+/*function everyinterval(n) {
+  if ((GameArea.frameNo / n) % 1 == 0) {return true;}
+  return false;
+}*/
+
+function renderBall(x,y,radius,color){
+  ctx = GameArea.context;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, 0, 2* Math.PI);
       ctx.fillStyle = color;
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-  }
+      ctx.fill();
+}
 
-  function createPlayer(x,y,radius,startAngle,endAngle,color){
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.startAngle = startAngle;
-    this.endAngle = endAngle;
-    this.update = function(){
-        ctx = GameArea.context;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
-        ctx.fillStyle = color;
-        ctx.fill();
-    }
-  }
-  
-  function renderBall(x,y,radius,color){
+function createWall(width, height, color, x, y) {
+  this.width = width;
+  this.height = height;
+  this.x = x;
+  this.y = y;
+  this.update = function(){
     ctx = GameArea.context;
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, 0, 2* Math.PI);
-        ctx.fillStyle = color;
-        ctx.fill();
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+function createPlayer(x,y,radius,startAngle,endAngle,color){
+  this.x = x;
+  this.y = y;
+  this.radius = radius;
+  this.startAngle = startAngle;
+  this.endAngle = endAngle;
+  this.velocityX = 0;
+  this.velocityY = 0;
+  this.update = function(){
+      ctx = GameArea.context;
+      ctx.beginPath();
+      ctx.arc(this.x, this.y, this.radius, this.startAngle, this.endAngle);
+      ctx.fillStyle = color;
+      ctx.fill();
+  }
+  this.newPos = function(){
+    this.x += this.velocityX;
+    this.y += this.velocityY;
   }
 
-  function updateGameArea() {
-    GameArea.clear();
-    //GamePiece.update();
-    renderBall(ball.x, ball.y, ball.radius, "red");
-    testWall.update();
-    testWall2.update();
+  this.addForce = function(forceX, forceY){
+    this.velocityX += forceX;
+    this.velocityY += forceY;
   }
+}
+
+function applyForce(forceX, forceY) {
+  this.velocityX += forceX;
+  this.velocityY += forceY;
+}
+
+function updatePosition() {
+  this.x += this.velocityX;
+  this.y += this.velocityY;
+}
+
+function moveup() {
+  //myGamePiece.speedY -= 1;
+  GamePiece.addForce(0,-1);
+}
+
+function movedown() {
+  //myGamePiece.speedY += 1;
+  GamePiece.addForce(0,1);
+}
+
+function moveleft() {
+  //myGamePiece.speedX -= 1;
+  GamePiece.addForce(-1,0);
+}
+
+function moveright() {
+  //myGamePiece.speedX += 1;
+  GamePiece.addForce(1,0);
+}
+
+
+function updateGameArea() {
+  GameArea.clear();
+  GamePiece.update();
+  GamePiece.newPos();
+  testWall.update();
+  testWall2.update();
+  
+}
